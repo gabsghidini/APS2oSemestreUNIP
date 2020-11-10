54b15204f915888877109e8f311eb0ce20cf7c14
gabsghidini <43341520+gabsghidini@users.noreply.github.com>
2020-11-09 22:07:07 -0300

    Fixed UI problems

--- UI/loginPage.py
+++ UI/loginPage.py
@@ -1,186 +1,104 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>500</height>
-   </rect>
-  </property>
-  <property name="maximumSize">
-   <size>
-    <width>800</width>
-    <height>500</height>
-   </size>
-  </property>
-  <property name="windowTitle">
-   <string>Sistema Naval: SS Grupo 10</string>
-  </property>
-  <property name="styleSheet">
-   <string notr="true">background-color: rgb(248, 248, 248)</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QLabel" name="loginTitle">
-    <property name="geometry">
-     <rect>
-      <x>30</x>
-      <y>20</y>
-      <width>361</width>
-      <height>31</height>
-     </rect>
-    </property>
-    <property name="font">
-     <font>
-      <family>Montserrat Medium</family>
-      <pointsize>16</pointsize>
-      <weight>7</weight>
-      <italic>false</italic>
-      <bold>false</bold>
-     </font>
-    </property>
-    <property name="styleSheet">
-     <string notr="true">font: 57 16pt &quot;Montserrat Medium&quot;;
-color: rgb(50, 79, 123);</string>
-    </property>
-    <property name="text">
-     <string>Sistema de Login - SS Grupo 10:</string>
-    </property>
-   </widget>
-   <widget class="QWidget" name="horizontalLayoutWidget">
-    <property name="geometry">
-     <rect>
-      <x>370</x>
-      <y>420</y>
-      <width>381</width>
-      <height>31</height>
-     </rect>
-    </property>
-    <layout class="QHBoxLayout" name="horizontalLayout">
-     <item>
-      <widget class="QPushButton" name="loginButton">
-       <property name="font">
-        <font>
-         <family>Montserrat SemiBold</family>
-         <pointsize>10</pointsize>
-         <weight>7</weight>
-         <italic>false</italic>
-         <bold>false</bold>
-        </font>
-       </property>
-       <property name="styleSheet">
-        <string notr="true">font: 63 10pt &quot;Montserrat SemiBold&quot;;
-background-color: rgb(134, 166, 223);
-color: rgb(248, 248, 248);</string>
-       </property>
-       <property name="text">
-        <string>Login</string>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="QPushButton" name="forgotUserButton">
-       <property name="font">
-        <font>
-         <family>Montserrat SemiBold</family>
-         <pointsize>10</pointsize>
-         <weight>7</weight>
-         <italic>false</italic>
-         <bold>false</bold>
-        </font>
-       </property>
-       <property name="styleSheet">
-        <string notr="true">font: 63 10pt &quot;Montserrat SemiBold&quot;;
-background-color: rgb(134, 166, 223);
-color: rgb(248, 248, 248);</string>
-       </property>
-       <property name="text">
-        <string>Esqueci meu Usuário</string>
-       </property>
-      </widget>
-     </item>
-    </layout>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit">
-    <property name="geometry">
-     <rect>
-      <x>120</x>
-      <y>80</y>
-      <width>311</width>
-      <height>31</height>
-     </rect>
-    </property>
-    <property name="styleSheet">
-     <string notr="true">font: 57 10pt &quot;Montserrat Medium&quot;;</string>
-    </property>
-    <property name="text">
-     <string/>
-    </property>
-   </widget>
-   <widget class="QLabel" name="emailLabel">
-    <property name="geometry">
-     <rect>
-      <x>30</x>
-      <y>80</y>
-      <width>71</width>
-      <height>36</height>
-     </rect>
-    </property>
-    <property name="styleSheet">
-     <string notr="true">font: 63 10pt &quot;Montserrat Medium&quot;;
-</string>
-    </property>
-    <property name="text">
-     <string>Usuário:</string>
-    </property>
-   </widget>
-   <widget class="QLabel" name="label">
-    <property name="geometry">
-     <rect>
-      <x>30</x>
-      <y>130</y>
-      <width>71</width>
-      <height>31</height>
-     </rect>
-    </property>
-    <property name="styleSheet">
-     <string notr="true">font: 63 10pt &quot;Montserrat Medium&quot;;</string>
-    </property>
-    <property name="text">
-     <string>Senha:</string>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit_2">
-    <property name="geometry">
-     <rect>
-      <x>120</x>
-      <y>130</y>
-      <width>311</width>
-      <height>31</height>
-     </rect>
-    </property>
-    <property name="styleSheet">
-     <string notr="true">font: 57 10pt &quot;Montserrat Medium&quot;;</string>
-    </property>
-    <property name="text">
-     <string/>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>20</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'loginPage.ui'
+#
+# Created by: PyQt5 UI code generator 5.15.1
+#
+# WARNING: Any manual changes made to this file will be lost when pyuic5 is
+# run again.  Do not edit this file unless you know what you are doing.
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 500)
+        MainWindow.setMaximumSize(QtCore.QSize(800, 500))
+        MainWindow.setStyleSheet("background-color: rgb(248, 248, 248)")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.loginTitle = QtWidgets.QLabel(self.centralwidget)
+        self.loginTitle.setGeometry(QtCore.QRect(30, 20, 361, 31))
+        font = QtGui.QFont()
+        font.setFamily("Montserrat Medium")
+        font.setPointSize(16)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(7)
+        self.loginTitle.setFont(font)
+        self.loginTitle.setStyleSheet("font: 57 16pt \"Montserrat Medium\";\n"
+"color: rgb(50, 79, 123);")
+        self.loginTitle.setObjectName("loginTitle")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(370, 420, 381, 31))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.loginButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Montserrat SemiBold")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(7)
+        self.loginButton.setFont(font)
+        self.loginButton.setStyleSheet("font: 63 10pt \"Montserrat SemiBold\";\n"
+"background-color: rgb(134, 166, 223);\n"
+"color: rgb(248, 248, 248);")
+        self.loginButton.setObjectName("loginButton")
+        self.horizontalLayout.addWidget(self.loginButton)
+        self.forgotUserButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Montserrat SemiBold")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(7)
+        self.forgotUserButton.setFont(font)
+        self.forgotUserButton.setStyleSheet("font: 63 10pt \"Montserrat SemiBold\";\n"
+"background-color: rgb(134, 166, 223);\n"
+"color: rgb(248, 248, 248);")
+        self.forgotUserButton.setObjectName("forgotUserButton")
+        self.horizontalLayout.addWidget(self.forgotUserButton)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(120, 80, 311, 31))
+        self.lineEdit.setStyleSheet("font: 57 10pt \"Montserrat Medium\";")
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.emailLabel = QtWidgets.QLabel(self.centralwidget)
+        self.emailLabel.setGeometry(QtCore.QRect(30, 80, 71, 36))
+        self.emailLabel.setStyleSheet("font: 63 10pt \"Montserrat Medium\";\n"
+"")
+        self.emailLabel.setObjectName("emailLabel")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 130, 71, 31))
+        self.label.setStyleSheet("font: 63 10pt \"Montserrat Medium\";")
+        self.label.setObjectName("label")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(120, 130, 311, 31))
+        self.lineEdit_2.setStyleSheet("font: 57 10pt \"Montserrat Medium\";")
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Sistema Naval: SS Grupo 10"))
+        self.loginTitle.setText(_translate("MainWindow", "Sistema de Login - SS Grupo 10:"))
+        self.loginButton.setText(_translate("MainWindow", "Login"))
+        self.forgotUserButton.setText(_translate("MainWindow", "Esqueci meu Usuário"))
+        self.emailLabel.setText(_translate("MainWindow", "Usuário:"))
+        self.label.setText(_translate("MainWindow", "Senha:"))
